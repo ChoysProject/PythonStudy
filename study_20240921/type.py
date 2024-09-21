fb2b5a40c1af552271395261d27b8fb783596956
choys <charm4860as@gmail.com>
2024-09-21 14:40:02 +0900

    first commit

---
+++ study_20240921/type.py
@@ -0,0 +1,10 @@
+
+
+
+    n1 = int(input("정수입력 : "))
+    n2 = int(input("정수입력 : "))
+    print(n1, '+', n2, '=', n1+n2)
+    print("%d+%d=%d" % (n1,n2, n1+n2))
+    print("%d+%d=%d" % (n1,n2, n1-n2))
+    print("%d+%d=%d" % (n1,n2, n1*n2))
+    print("%d+%d=%d" % (n1,n2, n1/n2))
