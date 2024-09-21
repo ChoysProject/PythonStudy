8bee8b20899f51fcca5953131cfe867effd73fe8
choys <charm4860as@gmail.com>
2024-09-21 15:50:14 +0900

    pythod hunny jam

---
+++ study_20240921/code02-01.py
@@ -0,0 +1,8 @@
+## 함수 선언 부분 ##
+def plus(v1,v2):
+    result = 0
+    result = v1+v2
+    return result
+
+print(plus(1,2))
+
---
+++ study_20240921/code02-04.py
@@ -0,0 +1,23 @@
+
+floadVar = 0.123456789012345678901234567890
+
+floadVar
+type(floadVar)
+
+###############
+
+alist = [10,20,30,40]
+
+print(alist[0])
+alist[1] = 200
+
+print(alist[1])
+print(alist)
+
+alist.append(50)
+print(alist[4])
+print(alist)
+
+alist.insert(5,5)
+
+print(alist)
